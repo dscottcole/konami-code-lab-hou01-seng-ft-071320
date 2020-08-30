cd9b7b119eaabf835e4b79d903cb2db1bddb17e8
dscottcole <67556122+dscottcole@users.noreply.github.com>
2020-08-29 19:23:39 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,31 @@ const codes = [
   "a"
 ];
 
+let index = 0;
+
 function init() {
-  // your code here
+  document.body.addEventListener('keydown', (e) => {
+      
+     const key = e.key;
+     console.log(key)
+    
+       if (key === codes[index]) {
+       index++;
+    
+       if (index === codes.length) {
+         alert("Hurray!");
+    
+         index = 0;
+       }
+     } else {
+       index = 0;
+    }
+  })
 }
+
+
+
+window.addEventListener('DOMContentLoaded', () => {
+  console.log('booted up')
+init()
+})
